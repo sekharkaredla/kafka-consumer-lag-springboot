03c3b29eebeeaf7317a994985a83cc2a58ce00aa
Sekhar Karedla <sekhar.karedla@gmail.com>
2022-08-18 01:10:43 -0400

    producer done

---
+++ event_producer/src/main/java/com/analyze/producer/producer/component/ProducerComponent.java
@@ -0,0 +1,47 @@
+package com.analyze.producer.producer.component;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Date;
+
+@Component
+public class ProducerComponent {
+
+    Logger logger = LoggerFactory.getLogger(ProducerComponent.class);
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    @Value("${kafka_topic_name}")
+    private String topicName;
+
+    private void sendEvent(String message) {
+        String finalMessage = message + " -- " + new Date();
+        logger.info("Sending Message : " + finalMessage);
+        this.kafkaTemplate.send(topicName, finalMessage);
+    }
+
+    public void sendEventsWithTimeDelay(String message, long waitTime) {
+        while (true) {
+            try {
+                Thread.sleep(waitTime);
+                sendEvent(message);
+            } catch (Exception e) {
+                e.printStackTrace();
+                return;
+            }
+        }
+    }
+
+    @PostConstruct
+    public void startPublishing() {
+        this.sendEventsWithTimeDelay("Producer", 3000);
+    }
+
+}
